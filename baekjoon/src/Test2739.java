7b7dde789622f359f580eb3a74c17e7bdea1e82f
Seohyun So <soseohyuni@gmail.com>
2021-11-05 00:23:39 +0900

    Test 2739 커밋

---
+++ baekjoon/src/Test2739.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Test2739 
+{
+	public static void main(String[] args)
+	{
+		Scanner sc = new Scanner(System.in);
+		
+		int a = sc.nextInt();
+		
+		for(int i=0; i<10; i++)
+		{
+			System.out.println(a + " * " + i + " = " + a*i);
+		}
+	}
+}
