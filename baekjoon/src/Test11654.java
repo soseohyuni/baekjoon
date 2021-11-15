a942cfa721c2583823e3eeff9cdf0ea692ff6a5a
Seohyun So <soseohyuni@gmail.com>
2021-11-15 22:15:50 +0900

    11654 커밋

---
+++ baekjoon/src/Test11654.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class Test11654
+{
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        
+        char ch = sc.next().charAt(0);
+        
+        int num = (int)ch;
+        
+        System.out.println(num);
+        
+        sc.close();
+    }
+}
