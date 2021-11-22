373238ffdb089c2ac13f5aebc97085294937abf3
Seohyun So <soseohyuni@gmail.com>
2021-11-22 23:31:28 +0900

    2775 수정

--- baekjoon/src/Test2775.java
+++ baekjoon/src/Test2775.java
@@ -10,26 +10,25 @@ public class Test2775
 		// 아파트 생성 
 		int[][] APT = new int[15][15];
  
-		for(int i = 0; i < 15; i++) 
+		for(int i=0; i<15; i++) 
 		{
 			APT[i][1] = 1;	// i층 1호
 			APT[0][i] = i;	// 0층 i호
 		}
  
  
-		for(int i = 1; i < 15; i ++) 
+		for(int i=1; i<15; i ++) 
 		{	// 1층부터 14층까지
- 
-			for(int j = 2; j < 15; j++) 
+			for(int j=2; j<15; j++) 
 			{	// 2호부터 14호까지
 				APT[i][j] = APT[i][j - 1] + APT[i - 1][j];
 			}
 		}
 		
-		// 테스트 부분 		
-		int T = sc.nextInt();
+		// 테스트 케이스
+		int t = sc.nextInt();
 		
-		for(int i = 0; i < T; i++) 
+		for(int i=0; i<t; i++) 
 		{
 			int k = sc.nextInt();
 			int n = sc.nextInt();
