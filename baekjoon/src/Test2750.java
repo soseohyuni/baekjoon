bbbde9929650cdf6aa57c24d8dfb15b81b8daab3
Seohyun So <soseohyuni@gmail.com>
2021-12-03 23:43:55 +0900

    #2750 수 정렬하기

---
+++ baekjoon/src/Test2750.java
@@ -0,0 +1,27 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Test2750 
+{
+	public static void main(String[] args)
+	{
+		Scanner sc = new Scanner(System.in);
+		
+		int n = sc.nextInt();
+		int[] arr = new int[n];
+		
+		for(int i=0; i<n; i++)
+		{
+			arr[i] = sc.nextInt();
+		}
+		
+		Arrays.sort(arr);
+		
+		for(int i : arr)
+		{
+			System.out.println(i);
+		}
+		
+		sc.close();
+	}
+}
