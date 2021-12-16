e106a16042d5a48ba6c89256571de6cf9805915d
Seohyun So <soseohyuni@gmail.com>
2021-12-16 23:41:17 +0900

    #10844 쉬운 계단 수

--- baekjoon/src/Test10844.java
+++ baekjoon/src/Test10844.java
@@ -1,29 +1,35 @@
 import java.util.Scanner;
  
-public class Test10844 {
-	
+public class Test10844 
+{
 	static Long[][] dp;
 	static int N;
 	final static long MOD = 1000000000;
 	
-	public static void main(String[] args) {
-		Scanner in = new Scanner(System.in);
+	public static void main(String[] args) 
+	{
+		Scanner sc = new Scanner(System.in);
 		
-		N = in.nextInt();
+		N = sc.nextInt();
 		dp = new Long[N + 1][10];
 		
 		// 첫번째 자릿수는 1로 초기화 
-		for(int i = 0; i < 10; i++) {
+		for(int i = 0; i < 10; i++) 
+		{
 			dp[1][i] = 1L;
 		}
 		
 		long result = 0;
 		
 		// 마지막 자릿수인 1~9까지의 경우의 수를 모두 더해준다.
-		for(int i = 1; i <= 9; i++) {
+		for(int i = 1; i <= 9; i++) 
+		{
 			result += recur(N, i);
 		}
+		
 		System.out.println(result % MOD);
+		
+		sc.close();
 	}
 	
 	/*
@@ -34,28 +40,26 @@ public class Test10844 {
 	 1로 초기화 되어있어야한다.
 	*/
 	
-	static long recur(int digit, int val) {		
- 
+	static long recur(int digit, int val) 
+	{		
 		// 첫째 자리수에 도착한다면 더이상 탐색할 필요 없음
-		if(digit == 1) {
+		if(digit == 1) 
 			return dp[digit][val];
-		}
 			
 		// 해당 자리수의 val값에 대해 탐색하지 않았을 경우 
-		if(dp[digit][val] == null) {
+		if(dp[digit][val] == null) 
+		{
 			// val이 0일경우 다음은 1밖에 못옴
-			if(val == 0) {
+			if(val == 0) 
 				dp[digit][val] = recur(digit - 1 ,1);
-			}
 			// val이 1일경우 다음은 8밖에 못옴
-			else if(val== 9) {
+			else if(val== 9) 
 				dp[digit][val] = recur(digit - 1, 8);
-			}
 			// 그 외의 경우는 val-1과 val+1 값의 경우의 수를 합한 경우의 수가 됨
-			else {
+			else 
 				dp[digit][val] = recur(digit - 1, val - 1) + recur(digit - 1, val + 1);
-			}
 		}
+		
 		return dp[digit][val] % MOD;
 	}
 }
