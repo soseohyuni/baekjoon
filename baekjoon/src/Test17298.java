562e426688434ffec22f23e2a4a0c8f0475bf365
Seohyun So <soseohyuni@gmail.com>
2022-02-03 00:18:47 +0900

    #17298 오큰수

--- baekjoon/src/Test17298.java
+++ baekjoon/src/Test17298.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+import java.util.Stack;
 
 public class Test17298 
 {
@@ -6,41 +7,41 @@ public class Test17298
 	{
 		Scanner sc = new Scanner(System.in);
 		StringBuilder sb = new StringBuilder();
+		Stack<Integer> stack = new Stack<Integer>();
 		
 		int N = sc.nextInt();
 		int[] arr = new int[N];
 		
-		for(int i=0; i<N; i++)
+		for(int i=0; i<N; i++) 
 		{
 			arr[i] = sc.nextInt();
 		}
-		
-		for(int i=0; i<N; i++)
+ 
+		for(int i=0; i<N; i++) 
 		{
-			if(i==N-1)
+			/*
+			 * 스택이 비어있지 않으면서 
+			 * 현재 원소가 스택의 맨 위 원소가 가리키는 원소보다 큰 경우
+			 * 해당 조건을 만족할 때 까지 stack의 원소를 pop하면서
+			 * 해당 인덱스의 값을 현재 원소로 바꿔준다. 
+			 */
+			while(!stack.isEmpty() && arr[stack.peek()] < arr[i]) 
 			{
-				sb.append("-1");
-				break;
+				arr[stack.pop()] = arr[i];
 			}
 			
-			int max = arr[i]; 
-			
-			for(int j=i+1; j<N; j++)
-			{
-				if(max<arr[j])
-				{
-					sb.append(arr[j] + " ");
-					break;
-				}
-				else
-				{
-					if(j==N-1)
-					{
-						sb.append("-1 ");
-						break;
-					}
-				}	
-			}
+			stack.push(i);
+		}
+
+		// 스택에 남아 있는 요소들을 pop하면서 -1 로 초기화
+		while(!stack.isEmpty()) 
+		{
+			arr[stack.pop()] = -1;
+		}
+		
+		for(int i=0; i<N; i++) 
+		{
+			sb.append(arr[i]).append(' ');
 		}
 		
 		System.out.println(sb);
