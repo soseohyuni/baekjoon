c193caaeaa983ca04c44614af8efb2a8cefdaf9f
Seohyun So <soseohyuni@gmail.com>
2022-02-07 23:56:17 +0900

    #5430 AC (미완성)

---
+++ baekjoon/src/Test5430.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Test5430 
+{
+	public static void main(String[] args)
+	{
+		Scanner sc = new Scanner(System.in);
+		
+		int T = sc.nextInt();
+		
+		while(T-- > 0)
+		{
+			String p = sc.next();
+			int n = sc.nextInt();
+			int[] arr = new int[n];
+			
+			
+		}
+		
+		sc.close();
+	}
+}
